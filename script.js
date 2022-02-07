3a3b2d604fcde33be369eb3f9c81151270c398fd
Victor Hugo Lessa <victor.lessa@estudante.ufla.br>
2022-02-07 15:50:27 -0300

    Codigo com alguns erros arrumados porém não final

--- script.js
+++ script.js
@@ -1,8 +1,45 @@
 let numberCards = 0;
 
+
+const arrayCards = [];
+
+
+//função para escolher numero de cartas
 function chooseNumberCards() {
-    while (numberCards < 4 || (numberCards % 2) != 0 || numberCards > 14) {
+    while (numberCards < 4 || (numberCards % 2) !== 0 || numberCards > 14) {
         numberCards = parseInt(prompt("Qual o número de cartas que deseja jogar? (Escolha entre 4 e 14 e somente números pares) "))
     }
 }
+
+
+function addCards() {
+    for (let i = 0; i < numberCards.length; i++) {
+        arrayCards.push = (`
+        <div class="card ">
+            <button class="front-face face">
+                <img src="img/front.png" alt="imagem papagaio">
+            </button>
+            <div class="back-face face">
+                <img src="${arrayCards[i]}" alt="teste"> 
+            </div>
+        </div>`);
+
+        arrayCards.push = (`
+        <div class="card ">
+            <button class="front-face face">
+                <img src="img/front.png" alt="imagem papagaio">
+            </button>
+            <div class="back-face face">
+                <img src="${arrayCards[i]}" alt="teste"> 
+            </div>
+        </div>`);
+    }
+
+
+}
+
 chooseNumberCards();
+addCards();
+/*cards = Array.from(document.getElementsByClassName("card"));
+
+addCards();*/
